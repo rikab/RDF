378ad1a6fa422cb77a94cfe996d314e5db3c9a73
Rikab <rikab@mit.edu>
2025-01-23 17:39:02 -0500

    complex factoring

--- jax/utils/function_utils.py
+++ jax/utils/function_utils.py
@@ -2,9 +2,15 @@ import numpy as np
 import jax.numpy as jnp
 import jax
 import math
+from scipy.signal import convolve2d
 
 
-# Necessary for jax to work with 0^0
+# #################################
+# ########## POLYNOMIALS ##########
+# #################################
+
+# Necessary for jax to work with 0^0@jax.jit
+
 def build_powers(base, length):
    
     def body_fun(i, arr):
@@ -19,6 +25,9 @@ def build_powers(base, length):
     arr = jax.lax.fori_loop(1, length, body_fun, arr)
     return arr
 
+
+
+@jax.jit
 def polynomial(t, alpha, params):
 
     M, N = params.shape
@@ -33,6 +42,49 @@ def polynomial(t, alpha, params):
     return poly_val
 
 
+def polynomial_multiply(c1, c2, M = None, N = None):
+
+    if M is None:
+        M = c1.shape[0] + c2.shape[0]
+    if N is None:
+        N = c1.shape[1] + c2.shape[1]
+
+    full_conv = convolve2d(c1, c2, mode='full')
+    truncated = full_conv
+    return truncated
+
+
+
+@jax.jit
+def polynomial_sum(cs):
+
+    # zero pad to the same size
+    max_M = max([c.shape[0] for c in cs])
+    max_N = max([c.shape[1] for c in cs])
+
+    for i in range(len(cs)):
+        M = cs[i].shape[0]
+        N = cs[i].shape[1]
+        cs[i] = jnp.pad(cs[i], ((0, max_M - M), (0, max_N - N)), mode='constant')
+
+    cs = jnp.array(cs)
+    return jnp.sum(cs, axis=0)
+
+
+def polynomial_power(c, k):
+    
+    if k == 0:
+        return jnp.array([[1,]])
+    elif k == 1:
+        return c
+    else:
+        return polynomial_multiply(c, polynomial_power(c, k-1))
+
+
+
+# ##############################
+# ########## CALCULUS ##########
+# ##############################
 
 
 def taylor_expand_in_alpha(function, order):
